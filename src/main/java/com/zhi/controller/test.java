52f8313b5869733285e3dc91b0027addd02f4f47
zhizhongjian <zhizhongjian@bingosoft.net>
2020-01-13 18:36:27 +0800

    增加api 北京时间：/

--- src/main/java/com/zhi/controller/test.java
+++ src/main/java/com/zhi/controller/test.java
@@ -14,4 +14,9 @@ public class test {
     public String test02(){
         return "广州市品高软件股份有限公司";
     }
+
+    @RequestMapping("/")
+    public String test03(){
+        return "北京时间2019/1/13 18:30";
+    }
 }
