cc402541efb118f71d102907c0f27a45beabf407
zhizhongjian <zhizhongjian@bingosoft.net>
2020-01-14 10:56:44 +0800

    2020/1/14

--- src/main/java/com/zhi/controller/test.java
+++ src/main/java/com/zhi/controller/test.java
@@ -19,4 +19,9 @@ public class test {
     public String test03(){
         return "北京时间2019/1/13 18:30";
     }
+
+    @RequestMapping("/test04")
+    public String test04() {
+        return "北京時間2020/1/1410：55";
+    }
 }
